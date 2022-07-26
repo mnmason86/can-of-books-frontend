b7e1416f9cf1c8328dff7367af80db07a65e99e1
Kyel <kboisseau93@gmail.com>
2022-07-26 14:25:35 -0700

    More changes

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -9,7 +9,20 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
-
+getBooks = async () => {
+  try {
+    console.log(`{SERVER}/books`)
+    let results = await axios.get(`${SERVER}/books`)
+    this.setState({
+      books: results.data
+    })
+  } catch (error) {
+    console.log('error')
+  }
+  componentDidMount () 
+    this.getBooks();
+  }
+}
   render() {
 
     /* TODO: render all the books in a Carousel */
